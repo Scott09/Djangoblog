f0789106f217ced5c153547abae39ec822be2379
Scott Appleton <scottappleton09@gmail.com>
2020-09-01 18:48:24 -0700

    Instead of just sending a response as a string. We are now rendering html templates on specific route requests

--- djangonautic/views.py
+++ djangonautic/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-  return HttpResponse("Homepage")
+ #return HttpResponse("Homepage")
+  return render(request, 'homepage.html')
 
 def about(request):
-  return HttpResponse("About")
-
+  #return HttpResponse("About")
+  return render(request, 'about.html')
